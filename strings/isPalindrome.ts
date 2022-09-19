90c01b0c2ca5e51fc05bdda8d0927de4ec6bc6f8
Jerica Tolentino <jericatolentino@outlook.com>
2022-09-19 10:40:30 +0100

    feat: isPalindrome second solution

--- strings/isPalindrome.ts
+++ strings/isPalindrome.ts
@@ -25,6 +25,19 @@ function isPalindrome(s: string): boolean {
     return true;
 };
 
+
+const isPalindrome2 = (input: string) => {
+    if (!input) return true
+
+    const sanitizeInput = Array.from(input).filter(character => character.match(/^[a-z0-9]+$/i)) //alphanumeric!
+    const isOdd = sanitizeInput.length % 2 !== 0
+
+    let firstHalf = sanitizeInput.slice(0, sanitizeInput.length/2)
+    let secondHalf = sanitizeInput.slice(isOdd ? sanitizeInput.length/2 + 1 : sanitizeInput.length/2, sanitizeInput.length)
+
+    return firstHalf.toString().toLowerCase().split(',').join() === secondHalf.reverse().toString().toLowerCase().split(',').join()
+}
+
 console.log(isPalindrome('ab2a'));
 
 /*
@@ -55,4 +68,8 @@ answer: true
 Case 7 - 
 input: "ab2a"
 answer: true
+
+Case 8 - 
+input: "0P"
+answer: false
 */
